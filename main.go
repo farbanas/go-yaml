5fb1beed89686f90707c6ca51d407a02ba19f9a0
arba <arbanasfran@gmail.com>
2020-02-04 11:42:56 +0100

    Initial commit

---
+++ main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func flagParser() (string, string, string){
+	action := flag.Arg(0)
+	flag
+	fp := flag.String("filePath", "", "Path to yaml file.")
+	key := flag.String("key", "", "Yaml key for the value to get/set. Ex. first.second will get " +
+		"the value of key second that is a subkey of first.")
+	var val string
+	if action == "set" {
+		val = *flag.String("value", "", "Value that you want to set.")
+	}
+	flag.Parse()
+	if *fp == "" || *key == "" {
+		if *fp == "" {
+			fmt.Println("Path to yaml file cannot be empty!")
+		}
+		if *key == "" {
+			fmt.Println("Yaml key cannot be empty!")
+		}
+		flag.PrintDefaults()
+		os.Exit(1)
+	}
+	return *fp, *key, val
+}
+
+func main() {
+	fp, key, _ := flagParser()
+	data := OpenFileRead(fp)
+	yamlData := ReadYaml(data)
+	GetValue(yamlData, key)
+}
---
+++ utils.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	yaml "gopkg.in/yaml.v2"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+)
+
+func OpenFileRead(path string) []byte {
+	f, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	data, err := ioutil.ReadAll(f)
+	if err != nil {
+		log.Fatal(err)
+	}
+	f.Close()
+	return data
+}
+
+func OpenFileWrite(path string) *os.File {
+	f, err := os.Create(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return f
+}
+
+func ReadYaml(data []byte) interface{} {
+	var out interface{}
+	err := yaml.Unmarshal(data, &out)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(out)
+	return out
+}
+
+func GetValue(yamlData interface{}, query string)	{
+	keys := strings.Split(query, ".")
+	for _, key := range keys {
+		yamlData = yamlData.(map[interface{}]interface{})[key]
+	}
+	yamlData = yamlData.([]interface{})[0].(string)
+	fmt.Println(yamlData)
+}
