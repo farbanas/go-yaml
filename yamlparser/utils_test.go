1d516a3db5f3d0b9e6291e453d1144c8a44035da
arba <arbanasfran@gmail.com>
2020-02-05 12:58:32 +0100

    Added report alloc

--- yamlparser/utils_test.go
+++ yamlparser/utils_test.go
@@ -18,16 +18,16 @@ func TestGetValue(t *testing.T) {
 		args args
 		want interface{}
 	}{
-		{"Get root value 1", args{yamlData:yamlData, query:"worry"}, "deer"},
-		{"Get root value 2", args{yamlData:yamlData, query:"satellites"}, "record"},
-		{"Get array value 1", args{yamlData:yamlData, query:"potatoes.0.soil.through"}, []interface{}{true, false, "station"}},
-		{"Get array value 2", args{yamlData:yamlData, query:"potatoes.1"}, false},
-		{"Get array value 3", args{yamlData:yamlData, query:"potatoes.2"}, true},
-		{"Get array value 4", args{yamlData:yamlData, query:"potatoes.0.soil.through.2"}, "station"},
-		{"Get leaf value 1", args{yamlData:yamlData, query:"potatoes.0.soil.engineer"}, true},
-		{"Get leaf value 2", args{yamlData:yamlData, query:"potatoes.0.soil.tip"}, "mind"},
-		{"Get leaf value 3", args{yamlData:yamlData, query:"potatoes.0.percent"}, true},
-		{"Get leaf value 3", args{yamlData:yamlData, query:"potatoes.0.stiff"}, "tight"},
+		{"Get root value 1", args{yamlData: yamlData, query: "worry"}, "deer"},
+		{"Get root value 2", args{yamlData: yamlData, query: "satellites"}, "record"},
+		{"Get array value 1", args{yamlData: yamlData, query: "potatoes.0.soil.through"}, []interface{}{true, false, "station"}},
+		{"Get array value 2", args{yamlData: yamlData, query: "potatoes.1"}, false},
+		{"Get array value 3", args{yamlData: yamlData, query: "potatoes.2"}, true},
+		{"Get array value 4", args{yamlData: yamlData, query: "potatoes.0.soil.through.2"}, "station"},
+		{"Get leaf value 1", args{yamlData: yamlData, query: "potatoes.0.soil.engineer"}, true},
+		{"Get leaf value 2", args{yamlData: yamlData, query: "potatoes.0.soil.tip"}, "mind"},
+		{"Get leaf value 3", args{yamlData: yamlData, query: "potatoes.0.percent"}, true},
+		{"Get leaf value 3", args{yamlData: yamlData, query: "potatoes.0.stiff"}, "tight"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -52,16 +52,16 @@ func TestGetValueReflect(t *testing.T) {
 		args args
 		want interface{}
 	}{
-		{"Get root value 1", args{yamlData:yamlData, query:"worry"}, "deer"},
-		{"Get root value 2", args{yamlData:yamlData, query:"satellites"}, "record"},
-		{"Get array value 1", args{yamlData:yamlData, query:"potatoes.0.soil.through"}, []interface{}{true, false, "station"}},
-		{"Get array value 2", args{yamlData:yamlData, query:"potatoes.1"}, false},
-		{"Get array value 3", args{yamlData:yamlData, query:"potatoes.2"}, true},
-		{"Get array value 4", args{yamlData:yamlData, query:"potatoes.0.soil.through.2"}, "station"},
-		{"Get leaf value 1", args{yamlData:yamlData, query:"potatoes.0.soil.engineer"}, true},
-		{"Get leaf value 2", args{yamlData:yamlData, query:"potatoes.0.soil.tip"}, "mind"},
-		{"Get leaf value 3", args{yamlData:yamlData, query:"potatoes.0.percent"}, true},
-		{"Get leaf value 3", args{yamlData:yamlData, query:"potatoes.0.stiff"}, "tight"},
+		{"Get root value 1", args{yamlData: yamlData, query: "worry"}, "deer"},
+		{"Get root value 2", args{yamlData: yamlData, query: "satellites"}, "record"},
+		{"Get array value 1", args{yamlData: yamlData, query: "potatoes.0.soil.through"}, []interface{}{true, false, "station"}},
+		{"Get array value 2", args{yamlData: yamlData, query: "potatoes.1"}, false},
+		{"Get array value 3", args{yamlData: yamlData, query: "potatoes.2"}, true},
+		{"Get array value 4", args{yamlData: yamlData, query: "potatoes.0.soil.through.2"}, "station"},
+		{"Get leaf value 1", args{yamlData: yamlData, query: "potatoes.0.soil.engineer"}, true},
+		{"Get leaf value 2", args{yamlData: yamlData, query: "potatoes.0.soil.tip"}, "mind"},
+		{"Get leaf value 3", args{yamlData: yamlData, query: "potatoes.0.percent"}, true},
+		{"Get leaf value 3", args{yamlData: yamlData, query: "potatoes.0.stiff"}, "tight"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -91,12 +91,13 @@ func TestSetValue(t *testing.T) {
 		})
 	}
 }
- */
+*/
 
 func BenchmarkGetValue(b *testing.B) {
 	data := OpenFileRead("test.yaml")
 	yamlData := ReadYaml(data)
 
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		GetValue(yamlData, "potatoes.0.soil.through.2")
 	}
@@ -106,6 +107,7 @@ func BenchmarkGetValueReflect(b *testing.B) {
 	data := OpenFileRead("test.yaml")
 	yamlData := ReadYaml(data)
 
+	b.ReportAllocs()
 	for i := 0; i < b.N; i++ {
 		GetValueReflect(yamlData, "potatoes.0.soil.through.2")
 	}
